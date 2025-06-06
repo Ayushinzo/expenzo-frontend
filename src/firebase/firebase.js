6504a804dd2851af7fd3ecf6a735dac7eeaef0ad
Ayush <ayushshembekar@gmail.com>
2025-06-06 14:00:14 +0530

    updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,26 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBQfgGoC8robkNQk0oV8tDZORtyB6lAZmE",
-//     authDomain: "first-app-746f8.firebaseapp.com",
-//     projectId: "first-app-746f8",
-//     storageBucket: "first-app-746f8.firebasestorage.app",
-//     messagingSenderId: "901333494462",
-//     appId: "1:901333494462:web:34fdf0a1afbbda45b24cef",
-//     measurementId: "G-Q8GND3KEYX"
-// };
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+    apiKey: "AIzaSyBQfgGoC8robkNQk0oV8tDZORtyB6lAZmE",
+    authDomain: "first-app-746f8.firebaseapp.com",
+    projectId: "first-app-746f8",
+    storageBucket: "first-app-746f8.firebasestorage.app",
+    messagingSenderId: "901333494462",
+    appId: "1:901333494462:web:34fdf0a1afbbda45b24cef",
+    measurementId: "G-Q8GND3KEYX"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
