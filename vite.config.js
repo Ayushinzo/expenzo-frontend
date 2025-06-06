c2b2a03fe1e5f1f7620f36b8e5ba82718f629e88
Ayush <ayushshembekar@gmail.com>
2025-06-06 13:42:20 +0530

    config updated

--- vite.config.js
+++ vite.config.js
@@ -7,33 +7,6 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['logo.png'],
-      manifest: {
-        name: 'Expenzo',
-        short_name: 'Expenzo',
-        description: 'A simple expense tracker app to manage your finances. Track your income and expenses, set budgets, and analyze your spending habits. Available on web, iOS, and Android. Sign in with Google or Facebook. Open source and free to use. Built with React, Tailwind CSS, and Vite.',
-        start_url: '/',
-        orientation: 'portrait',
-        scope: '/',
-        display: 'standalone',
-        background_color: 'white',
-        theme_color: 'white',
-        icons: [
-          {
-            src: '/logo-192-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/logo-512-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    tailwindcss()
   ],
 })
